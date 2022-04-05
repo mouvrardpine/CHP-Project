2acc9203273a1d3b55f17a0e8489f1b3e9d79ce2
lpianoka <80355494+lpianoka@users.noreply.github.com>
2022-04-05 17:59:02 +0200

    Add files via upload

--- matrix_RHS.h
+++ matrix_RHS.h
@@ -2,9 +2,9 @@
 
 #include <string>
 #include <vector>
-#include <cmath>
 
 std::vector<double> matvec(double dt,int Nx,int Ny,std::vector<double> x);
+std::vector<double> RHS( double dt, int Nx, int Ny, std::vector<double> u, double t);
 
 #define _MATRIX_RHS_H
 #endif
