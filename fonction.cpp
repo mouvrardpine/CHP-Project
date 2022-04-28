d163b38f03db595cba6d4bcc41b0ac29461bba72
lpianoka <80355494+lpianoka@users.noreply.github.com>
2022-04-28 18:33:32 +0200

    Add files via upload
    commenté whereme

--- fonction.cpp
+++ fonction.cpp
@@ -15,7 +15,7 @@ double fonctions :: f1(double x, double y, double t)
 {
     if (_pb==1)
     {
-        return 2*(y-y*y+x-x*x); 
+        return 2*(y-y*y+x-x*x);
     }
     else if (_pb==2)
     {
@@ -32,7 +32,7 @@ double fonctions :: g1(double x, double y, double t)
 {
     if (_pb==1)
     {
-        return 0; 
+        return 0;
     }
     else if (_pb==2)
     {
@@ -49,7 +49,7 @@ double fonctions :: h1(double x, double y, double t)
 
     if (_pb==1)
     {
-        return 0; 
+        return 0;
     }
     else if (_pb==2)
     {
@@ -65,8 +65,8 @@ double fonctions :: uex(double x, double y)
 {
     if (_pb==1)
     {
-        
-        return x*(1-x)*y*(1-y); 
+
+        return x*(1-x)*y*(1-y);
     }
     else if (_pb==2)
     {
@@ -84,7 +84,7 @@ charge_ :: charge_ (int n) : _n(n)
     MPI_Comm_rank(MPI_COMM_WORLD,&_me);
     MPI_Comm_size(MPI_COMM_WORLD,&_Np);
     charge( &_i1, &_iN, _me, _n, _Np);
-    
+
 
     /* int r = _n%_Np;
     if (_me<r)
@@ -93,7 +93,7 @@ charge_ :: charge_ (int n) : _n(n)
         _iN= _i1 + (_n/_Np+1)-1;
 
     }
-    else 
+    else
     {
         _i1=r+_me*(_n/_Np);
         _iN=_i1+(_n/_Np)-1;
@@ -109,23 +109,23 @@ void charge_ :: charge(int *i1,int *iN, int me, int n, int Np)
     *iN= *i1 + (n/Np+1)-1;
 
   }
-  else 
+  else
   {
     *i1=r+me*(n/Np);
     *iN=*i1+(n/Np)-1;
-  }    
+  }
 }
 
-int charge_ :: whichMe(int i)
-{
-    int i1, iN;
-    for (int k(0); k<_Np;k++)
-    {
-        charge(&i1,&iN,k,_n,_Np);
-        if (( i<=iN )&&(i>= i1))
-        {
-            return k;   
-        }
-    }
-    std::cout<<"erreur dans WhichME"<<std::endl;
-}
+// int charge_ :: whichMe(int i)
+// {
+//     int i1, iN;
+//     for (int k(0); k<_Np;k++)
+//     {
+//         charge(&i1,&iN,k,_n,_Np);
+//         if (( i<=iN )&&(i>= i1))
+//         {
+//             return k;
+//         }
+//     }
+//     std::cout<<"erreur dans WhichME"<<std::endl;
+// }
